e7772e8b066b002e45f3672f147f011a407afad1
Hariharan S <x23201550@student.ncirl.ie>
2024-03-13 19:10:44 +0000

    import fix

--- jobhunt/settings.py
+++ jobhunt/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from django.contrib import messages
-import os
 from django.contrib.auth import get_user_model
 
 if os.environ.get('CREATE_SUPERUSER'):
