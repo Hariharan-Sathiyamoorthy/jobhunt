123a068c651a64f50cb9fb9ae5da1706e99297a9
Hariharan S <x23201550@student.ncirl.ie>
2024-03-13 16:08:01 +0000

    removed whitespaces

--- jobhunt/settings.py
+++ jobhunt/settings.py
@@ -27,11 +27,9 @@ SECRET_KEY = 'django-insecure-rac5&(@j7!#jdzeyl&p95ca8d$(+@cnntpl%lvtf@_*00s2@cz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  [ 
-'localhost',
+ALLOWED_HOSTS = ['localhost',
 'x23201550labenv.eba-f854fb8j.eu-west-1.elasticbeanstalk.com',
-'http://127.0.0.1:8001'
-]
+'http://127.0.0.1:8001']
 
 
 # Application definition
